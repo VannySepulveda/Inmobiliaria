9c7995fb50e3eea61a894bbdaa11a26cb911c971
Valentina Sepulveda <vannysepulveda@gmail.com>
2024-05-23 18:05:42 -0400

    modificacion del admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Propiedad)
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(Solicitud_Arriendo)
